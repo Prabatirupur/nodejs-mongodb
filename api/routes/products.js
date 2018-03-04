4715388119222f795dbf77533bb36b346f2ff31e
velu <velu259@gmail.com>
2018-03-04 22:53:04 +0530

    include multer package

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const multer = require('multer');
+const upload = multer({dest: '/uploads'});
 const Product = require('../models/product');
 router.get('/', (req,res, next)=>{
     Product.find()
